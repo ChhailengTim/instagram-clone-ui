eb694900d1f6b4e6934414e2ce6cc664b204b7ed
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-03-18 23:42:08 +0700

    fix mobile responsive

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:instagram/utils/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }

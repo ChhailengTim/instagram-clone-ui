363cab75fce6ee11c02fada50c0077786b1b287c
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-03-19 16:29:28 +0700

    update

--- instagram/lib/screen/login_screen.dart
+++ instagram/lib/screen/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram/utils/colors.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -11,7 +13,26 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("From Login Screen"),
-    );
+        body: SafeArea(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            //svg image
+            SvgPicture.asset(
+              "assets/ic_instagram.svg",
+              color: primaryColor,
+              height: 64,
+            )
+            //Text field input for email
+            //text field input for password
+            //button login
+            //Transitioning to signing up
+          ],
+        ),
+      ),
+    ));
   }
 }

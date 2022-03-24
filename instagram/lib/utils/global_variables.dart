0e40f6cfdf6ae05a62110b1edbe4b9144ae8a543
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-03-25 00:05:54 +0700

    Create global_variables.dart

---
+++ instagram/lib/utils/global_variables.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/screen/add_post_screen.dart';
+
+const webScreenSize = 600;
+
+const homeScreenItems = [
+  Text("feed"),
+  Text("search"),
+  AddPostScreen(),
+  Text("favorite"),
+  Text("profile"),
+];

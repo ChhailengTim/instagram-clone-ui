7aa4e36eed5246dc1f02b3a299946e015602ea70
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-03-29 00:07:55 +0700

    Update feed_screen.dart

--- instagram/lib/screen/feed_screen.dart
+++ instagram/lib/screen/feed_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:instagram/utils/colors.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram/widgets/post_card.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({Key? key}) : super(key: key);
@@ -24,7 +25,23 @@ class FeedScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const PostCard(),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) => PostCard(
+              snap: snapshot.data!.docs[index].data(),
+            ),
+          );
+        },
+      ),
     );
   }
 }

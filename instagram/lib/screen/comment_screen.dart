e50b62a49a72d2f2ad6cd44a6a6177f3d1f1c685
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-03-30 15:15:11 +0700

    Update comment_screen.dart

---
+++ instagram/lib/screen/comment_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/utils/colors.dart';
+
+class CommentScreen extends StatefulWidget {
+  const CommentScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CommentScreen> createState() => _CommentScreenState();
+}
+
+class _CommentScreenState extends State<CommentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: const Text("Comment"),
+        centerTitle: false,
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          height: kToolbarHeight,
+          margin: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          padding: const EdgeInsets.only(left: 16, right: 8),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d"),
+                radius: 18,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
